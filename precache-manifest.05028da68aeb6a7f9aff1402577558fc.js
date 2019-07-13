c87d63d41cb9a226d9b21405887fc03a71600509
Sim Dim <simdim555@gmail.com>
2019-07-13 15:24:13 +0300

    Updates

---
+++ precache-manifest.05028da68aeb6a7f9aff1402577558fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a286da5b83edf5a4d3845f3bcb3dacfb",
+    "url": "/react-table-factory/index.html"
+  },
+  {
+    "revision": "5b7fe6860e7b9a10063d",
+    "url": "/react-table-factory/static/css/main.5f807200.chunk.css"
+  },
+  {
+    "revision": "90ec37bddca4960b8a6e",
+    "url": "/react-table-factory/static/js/2.d5ad02fd.chunk.js"
+  },
+  {
+    "revision": "5b7fe6860e7b9a10063d",
+    "url": "/react-table-factory/static/js/main.de8b5969.chunk.js"
+  },
+  {
+    "revision": "12bd98c636c8ee4ef69e",
+    "url": "/react-table-factory/static/js/runtime~main.8fcdf084.js"
+  }
+]);
